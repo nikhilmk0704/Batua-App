959fad7a32d13ae7fce90a9d402d56907c84735b
Varsha Udasi <varsha.udasi@tecsolsotware.com>
2016-03-31 15:46:32 +0530

    bower.json file updated including app.js and index.html file

--- assets/app/scripts/app.js
+++ assets/app/scripts/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ui.router', 'ui.bootstrap', 'toastr', 'angular-loading-bar', 'ngAnimate', 'ngCookies', 'ngLodash', 'httpi', 'datatables'])
+        .module('app', ['ui.router', 'ui.bootstrap', 'toastr', 'angular-loading-bar', 'ngAnimate', 'ngCookies', 'ngLodash', 'httpi', 'datatables', 'ngSanitize', 'flow', 'angucomplete-alt'])
         .config(config)
         .run(run);
 
