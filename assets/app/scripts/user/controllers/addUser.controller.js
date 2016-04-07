d395c965ba02ba2e16a17791b7dec22e1c41c656
Varsha Udasi <varsha.udasi@tecsolsotware.com>
2016-04-07 20:59:06 +0530

    add new user integration

--- assets/app/scripts/user/controllers/addUser.controller.js
+++ assets/app/scripts/user/controllers/addUser.controller.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('addUserController', ['$state', 'userService', 'toastr', function($state, userService, toastr) {
+angular.module('app').controller('addUserController', ['$state', '$scope', '$timeout', 'userService', 'toastr', 'imageUpload', function($state, $scope, $timeout, userService, toastr, imageUpload) {
 
     var vm = this;
 
@@ -15,6 +15,51 @@ angular.module('app').controller('addUserController', ['$state', 'userService', 
             return toastr.error(response.data);
         });
     };
+
+    /* --- START Image Upload --- */
+
+        vm.profileImageUpload = function(file, event, $flow) {
+            var file, image;
+            var imgOld = vm.editMerchantData.profileImageUrl;
+            var _URL = window.URL || window.webkitURL;
+            if ((file = file.file)) {
+                image = new Image();
+                image.onload = function() {
+                    $scope.$apply(function() {
+                        vm.editMerchantData.profileImageUrl = imgOld;
+                    });
+                    if (this.width < 320 || this.height < 240) {
+                        return toastr.error("Please select an image above 320px width and 240px height");
+                    }
+                    return handleProfileImageUpload(file, event, $flow);
+                }
+                image.onerror = function() {
+                    return toastr.error("not a valid file: " + file.type);
+                };
+                image.src = _URL.createObjectURL(file);
+            }
+        };
+
+        var handleProfileImageUpload = function(file, event, $flow) {
+            imageUpload.uploadImage(file, function(response) {
+                $flow.files = [];
+                if (response.status === 200) {
+                    $timeout(function() {
+                        $scope.$apply(function() {
+                            vm.editMerchantData.profileImageUrl = response.data;
+                        });
+                        vm.editMerchantData.profileImageUrl = response.data;
+                        return vm.editMerchantData.profileImageUrl;
+                    }, 1500);
+                }
+                if (response.status === 400) {
+                    return toastr.error(response.data.errors[0].message);
+                }
+                return toastr.error(response.data);
+            });
+        };
+
+        /* --- END Image Upload --- */
 
 
 }]);
