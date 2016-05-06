7d4f045e79f7b9aa5c7c5c5afcad860d0e9f3a90
vikash baghel <vikash.baghel@tecsolsoftware.com>
2016-05-06 17:06:53 +0530

    error file modified

--- api/errors/error.js
+++ api/errors/error.js
@@ -1,25 +1,31 @@
-module.exports={
+var _ = require('lodash');
 
-	send:function(errorParams){
-		var messageObject={};
-        if(typeof errorParams == "string")
-            messageObject.message=errorParams;
-        if(typeof errorParams=="object"){
-            var isErrorsArrayExist=(errorParams.errors[0].message)?(true):(false);
-            if(isErrorsArrayExist)
-                var messageError=(_.pick(errorParams.errors[0],'message').message);
-            if(!isErrorsArrayExist)
-                var messageError=(_.pick(errorParams,'message').message);
-            var exceptionError=(_.pick(errorParams,'exception'));
-            var contextError=(_.pick(errorParams,'error@context'));
-            var sqlError="Unable to perform operation";
-            messageObject.message = messageError||exceptionError||contextError||sqlError;
+module.exports = {
+
+    send: function(errorParams) {
+        var messageObject = {};
+        if (typeof errorParams == "string")
+            messageObject.message = errorParams;
+        if (typeof errorParams == "object") {
+            var isErrorsArrayExist = (errorParams.errors[0].message) ? (true) : (false);
+            var isUniqueVoilation = (_.indexOf(errorParams.errors[0].type.split(' '), 'unique') >= 0) ? (true) : (false);
+            var messageError;
+            if(isErrorsArrayExist && isUniqueVoilation)
+                messageError=Object.keys(errorParams.fields)[0]+" already exist";
+            if (isErrorsArrayExist && !isUniqueVoilation)
+                messageError = (_.pick(errorParams.errors[0], 'message').message);
+            if (!isErrorsArrayExist)
+                messageError = (_.pick(errorParams, 'message').message);
+            var exceptionError = (_.pick(errorParams, 'exception'));
+            var contextError = (_.pick(errorParams, 'error@context'));
+            var sqlError = "Unable to perform operation";
+            messageObject.message = messageError || exceptionError || contextError || sqlError;
         }
-        var array=[];
+        var array = [];
         array.push(messageObject);
-        var errorObject={};
-        errorObject.errors=array;
+        var errorObject = {};
+        errorObject.errors = array;
         return errorObject;
-	}
-	
+    }
+
 }
