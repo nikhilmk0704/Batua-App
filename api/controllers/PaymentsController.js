e99a18c3cbebadaaf3dced62377eb45a0b2d67ea
vikash baghel <vikash.baghel@tecsolsoftware.com>
2016-05-17 15:56:25 +0530

    transaction history and walle api for user app

--- api/controllers/PaymentsController.js
+++ api/controllers/PaymentsController.js
@@ -16,14 +16,47 @@ module.exports = {
         var params = req.body;
 
         var paymentService = new PaymentService();
-   
+
         paymentService.save(params, function(err, result) {
             if (err)
                 return res.badRequest(error.send(err));
-            if(_.isEmpty(result)){
+            if (_.isEmpty(result)) {
                 return res.notFound(error.send("Invalid promocode"));
             }
             return res.json(200, result);
         });
+    },
+
+    history: function(req, res) {
+
+        var params = {};
+        params.userId = req.param('userId');
+
+        var paymentService = new PaymentService();
+
+        paymentService.history(params, function(err, result) {
+            if (err)
+                return res.badRequest(error.send(err));
+            if (_.isEmpty(result))
+                return res.notFound(error.send('No Transactions'));
+            return res.json(200, result);
+        });
+    },
+
+    wallet: function(req, res) {
+
+        var params = {};
+        params.userId = req.param('userId');
+
+        var paymentService = new PaymentService();
+        
+        paymentService.wallet(params, function(err, result) {
+            if (err)
+                return res.badRequest(error.send(err));
+            if (_.isEmpty(result))
+                return res.notFound(error.send('No Transactions'));
+            return res.json(200, result);
+        });
     }
+
 };
